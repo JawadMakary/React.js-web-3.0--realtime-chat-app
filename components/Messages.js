21bcf3c2c0742b4816853e7ffc4756e9cd293f94
jawad <jawadhalbymakary@gmail.com>
2021-12-20 01:31:45 +0200

    setup useMoralisQuery

--- components/Messages.js
+++ components/Messages.js
@@ -1,9 +1,33 @@
 import { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
 import SendMessage from "./SendMessage";
+const MIN_DURATION = 15;
 function Messages() {
   const { user } = useMoralis();
   const endOfMessagesRef = useRef(null);
+
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - MIN_DURATION * 60 * 1000)
+        ),
+    [],
+    {
+      live: true,
+    }
+  );
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className="pb-56">
       <div className="my-5">
